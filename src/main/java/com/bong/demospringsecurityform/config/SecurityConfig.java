415e31e820b4fc478b6436d9e14189dc47f3da5b
cyr9210 <jipsyer@naver.com>
2021-06-21 09:07:13 +0900

    AccessDecisionManger customizing

--- src/main/java/com/bong/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/bong/demospringsecurityform/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.bong.demospringsecurityform.config;
 import com.bong.demospringsecurityform.domain.Account.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -20,11 +22,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.authorizeRequests()
         .mvcMatchers("/", "/info", "/account/**").permitAll()
         .mvcMatchers("/admin").hasRole("ADMIN")
+        .mvcMatchers("/users").hasRole("USER")
         .anyRequest().authenticated()
+        .expressionHandler(expressionHandler())
         .and()
         .formLogin()
         .and()
         .httpBasic();
+  }
+
+  private DefaultWebSecurityExpressionHandler expressionHandler() {
+    RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+    roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+    DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+    handler.setRoleHierarchy(roleHierarchy);
+    return handler;
   }
 
   @Override
